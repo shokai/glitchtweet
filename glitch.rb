3a852b87ccc4e826a9ab34781e37088530825fb9
Sho Hashimoto <hashimoto@shokai.org>
2010-07-31 01:54:42 +0900

    plugin cmabridge

--- glitch.rb
+++ glitch.rb
@@ -84,52 +84,8 @@ class Glitch
     return str.tr('あ-んア-ン', 'ア-ンあ-ん')
   end
 
-  def str_cmabridge(str)
-    require 'MeCab' rescue return str
-    strs = MeCab::Tagger.new.parse(str).map{|i|
-      i.split(/\t/).first
-    }.delete_if{|i|
-      i =~ /^EOS/
-    }.map{|i|
-      tmp = i.split(//u)
-      if tmp.size > 2
-        a = rand(tmp.size-1)
-        b = a+1
-        x = tmp[a]
-        tmp[a] = tmp[b]
-        tmp[b] = x
-        res = tmp.join('')
-      else
-        res = i
-      end
-      res
-    }
-    for i in 0...strs.size-1 do
-      if strs[i] =~ /^[ -~｡-ﾟ]*$/ and strs[i+1] =~ /^[ -~｡-ﾟ]*$/
-        strs[i] = strs[i]+' '
-      end
-    end
-    strs.join('')
-  end
-
   def str_line_prefix(str)
     '─'*3 + '─'*rand(6)+str
   end
   
-  def str_kirakira(str)
-    arr = "☆ .。.:* .。.:*･ﾟ∞∮+'∮+'★*｡.::･'ﾟ｡.::･'ﾟ｡.:*:･'ﾟ".split(//u)
-    count = 0
-    last = nil
-    kira = ""
-    loop do
-      c = arr[rand(arr.size)]
-      next if last == c
-      kira += c
-      last = c
-      count += 1
-      break if count > 8 and rand < 0.1
-    end
-    kira + str + kira.split(//u).reverse.join('')
-  end
-
 end
---
+++ plugins/cmabridge.rb
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+module Cmabridge
+  def Cmabridge.glitch(str)
+    require 'MeCab' rescue return str
+    strs = MeCab::Tagger.new.parse(str).map{|i|
+      i.split(/\t/).first
+    }.delete_if{|i|
+      i =~ /^EOS/
+    }.map{|i|
+      tmp = i.split(//u)
+      if tmp.size > 2
+        a = rand(tmp.size-1)
+        b = a+1
+        x = tmp[a]
+        tmp[a] = tmp[b]
+        tmp[b] = x
+        res = tmp.join('')
+      else
+        res = i
+      end
+      res
+    }
+    for i in 0...strs.size-1 do
+      if strs[i] =~ /^[ -~｡-ﾟ]*$/ and strs[i+1] =~ /^[ -~｡-ﾟ]*$/
+        strs[i] = strs[i]+' '
+      end
+    end
+    strs.join('')
+  end
+end
