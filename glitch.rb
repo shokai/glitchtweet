dfc1206dede6b0fb88e346173035e0e57ab8f138
Sho Hashimoto <hashimoto@shokai.org>
2010-07-30 16:23:20 +0900

    fix cmabridge

--- glitch.rb
+++ glitch.rb
@@ -71,15 +71,15 @@ class Glitch
 
   def str_cmabridge(str)
     require 'MeCab'
-    MeCab::Tagger.new.parse(str).map{|i|
+    strs = MeCab::Tagger.new.parse(str).map{|i|
       i.split(/\t/).first
     }.delete_if{|i|
       i =~ /^EOS/
     }.map{|i|
       tmp = i.split(//u)
       if tmp.size > 2
-        a = rand(tmp.size)
-        b = rand(tmp.size)
+        a = rand(tmp.size-1)
+        b = a+1
         x = tmp[a]
         tmp[a] = tmp[b]
         tmp[b] = x
@@ -89,6 +89,16 @@ class Glitch
       end
       res
     }
+    for i in 0...strs.size-1 do
+      if strs[i] =~ /^[ -~｡-ﾟ]*$/ and strs[i+1] =~ /^[ -~｡-ﾟ]*$/
+        strs[i] = strs[i]+' '
+      end
+    end
+    strs.join('')
+  end
+
+  def str_shuffle_words(str)
+    
   end
 
   def str_line_prefix(str)
