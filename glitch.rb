ef0a8629becf21ef5a900516e94f1c0482249701
Sho Hashimoto <hashimoto@shokai.org>
2010-07-30 23:10:12 +0900

    add kirakira

--- glitch.rb
+++ glitch.rb
@@ -107,4 +107,20 @@ class Glitch
     '─'*rand(6)+str
   end
   
+  def str_kirakira(str)
+    arr = "☆ .。.:* .。.:*･ﾟ∞∮+'∮+'★*｡.::･'ﾟ｡.::･'ﾟ｡.:*:･'ﾟ".split(//u)
+    count = 0
+    last = nil
+    kira = ""
+    loop do
+      c = arr[rand(arr.size)]
+      next if last == c
+      kira += c
+      last = c
+      count += 1
+      break if count > 8 and rand < 0.1
+    end
+    kira + str + kira.split(//u).reverse.join('')
+  end
+
 end
